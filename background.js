d758f2a2cb48a93f7656b435d06a681f785a9691
Jamie Nguyen <j@jamielinux.com>
2019-01-06 07:07:42 +0000

    Remove webNavigation permission

--- background.js
+++ background.js
@@ -3,30 +3,27 @@ let defaultZoom = 1;
 let gettingDefaultZoom = browser.storage.local.get("defaultZoom");
 gettingDefaultZoom.then((storage) => {
   if (storage.defaultZoom) {
-    defaultZoom = storage.defaultZoom;
+    defaultZoom = parseFloat(storage.defaultZoom);
   }
 });
 
-function setZoom(details) {
-  let gettingZoom = browser.tabs.getZoom(details.id);
-  gettingZoom.then((zoomFactor) => {
-    if (zoomFactor != defaultZoom) {
-      if (zoomFactor == 1) {
-        browser.tabs.setZoom(details.id, parseFloat(defaultZoom));
+function setZoom(tabId, changeInfo, tab) {
+  if (changeInfo.status) {
+    let gettingZoom = browser.tabs.getZoom(tabId);
+    gettingZoom.then((currentZoom) => {
+      if (defaultZoom != 1 && currentZoom == 1) {
+        browser.tabs.setZoom(tabId, defaultZoom);
       }
-    }
-  });
+    });
+  }
 }
 
 browser.storage.onChanged.addListener((newSettings) => {
-  defaultZoom = newSettings.defaultZoom.newValue
+  defaultZoom = parseFloat(newSettings.defaultZoom.newValue);
 });
 
-browser.tabs.onCreated.addListener((details) => {
-  browser.tabs.setZoom(details.id, parseFloat(defaultZoom));
+browser.tabs.onCreated.addListener((tab) => {
+  browser.tabs.setZoom(tab.id, defaultZoom);
 });
 
-browser.tabs.onActivated.addListener(setZoom);
-browser.webNavigation.onCommitted.addListener(setZoom);
-browser.webNavigation.onDOMContentLoaded.addListener(setZoom);
-browser.webNavigation.onCompleted.addListener(setZoom);
+browser.tabs.onUpdated.addListener(setZoom);
